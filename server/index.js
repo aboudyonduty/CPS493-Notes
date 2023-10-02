70ea77c3d17f8b5c70c478e0982d02904d1d4599
aboudyonduty <aoali726@gmail.comgit config --global user.email aoali726@gmail.comgit config --global user.email aoali726@gmail.com>
2023-10-02 14:06:27 -0400

    09/28/2023 Class Edits

---
+++ server/index.js
@@ -0,0 +1,17 @@
+//vanilla node server
+const http = require('http');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/',(req, res)=>{
+    res.end('Hello World!\n');
+});
+
+console.log('1: Trying to start server...');
+
+server.listen(PORT,()=>{
+    console.log(`2: Server is running at http://localhost:${PORT}`);
+});
+
+console.log('3: End of file, waiting for requests...');
